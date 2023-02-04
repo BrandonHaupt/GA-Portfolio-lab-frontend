daaecaa3a34c7d1f9412e52e5a39ae0a4ad68656
Brandon Haupt <haupt.brandon@gmail.com>
2023-02-04 15:22:13 -0600

    Update 1.1
    - added image to header
    - changed styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ export default function Header(props){
 
     return(
         <header>
+            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F64.media.tumblr.com%2Fa39911173ad8fd36f451961c23f504b9%2Ftumblr_of1p3313Al1rldv4go1_1280.gif&f=1&nofb=1&ipt=0bee84832b687b114a5f8b5ee78be70e11c5e37c102491eca4779bafc28e9239&ipo=images" alt="profile" className="header-img"/>
             <img src="./img/Profile-img.png" alt="profile" className="profile-img"/>
             <nav className="nav">
                 <Link to="/">
