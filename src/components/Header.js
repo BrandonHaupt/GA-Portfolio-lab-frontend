fe6e92cd5ce06cbf9df2a1bd7270a35c6e0bf44f
Brandon Haupt <haupt.brandon@gmail.com>
2023-02-06 10:48:33 -0600

    Update Header
    - added profile img

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
+import ProfilePicture from "./img/Profile-picture.png"
 
 export default function Header(props){
 
     return(
         <header>
             <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F64.media.tumblr.com%2Fa39911173ad8fd36f451961c23f504b9%2Ftumblr_of1p3313Al1rldv4go1_1280.gif&f=1&nofb=1&ipt=0bee84832b687b114a5f8b5ee78be70e11c5e37c102491eca4779bafc28e9239&ipo=images" alt="profile" className="header-img"/>
-            <img src="./img/Profile-img.png" alt="profile" className="profile-img"/>
+
+            <img src={ProfilePicture} alt="profile" className="profile-img"/>
             <nav className="nav">
                 <Link to="/">
                      <div>HOME</div>
