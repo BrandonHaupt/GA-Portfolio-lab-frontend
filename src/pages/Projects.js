4d739497098253abbea4408b05c4cf42ad00b0a6
Brandon Haupt <haupt.brandon@gmail.com>
2023-02-06 11:47:51 -0600

    Setting up projects page
    - changed scss to fit design

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,18 +4,24 @@ import { useLoaderData } from "react-router-dom";
 function Projects(props) {
   const projects = useLoaderData()
 
-    return projects.map((project) => (
-      <div>
-        <h1>{project.name}</h1>
-        <img src={project.image} alt={project.name}/>
-        <a href={project.git}>
-          <button>Github</button>
-        </a>
-        <a href={project.live}>
-          <button>live site</button>
-        </a>
+    return (
+      <div className="project">
+        {projects.map((project) => (
+          <div className="project-list">
+            <h1>{project.name}</h1>
+            <img src={project.image} alt={project.name}/>
+            <div className="project-buttons">
+              <a href={project.git}>
+                <button>Github</button>
+              </a>
+              <a href={project.live}>
+                <button>live site</button>
+              </a>
+            </div>
+          </div>
+        ))}
       </div>
-    ));
+    )
 }
 
 export default Projects;
